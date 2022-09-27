991f8c1ad17074ee3405d4e24bbebc5d96baaf9b
marco aurélio <marcoaurelio250303@gmail.com>
2022-09-27 14:02:00 -0300

    ajustes

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero,string imei, string memoria) : base(numero, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando ");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+var fone = new Nokia("3323131");
+fone.Ligar();
+fone.ReceberLigacao();
+fone.InstalarAplicativo("google");
+
+var foneMaca = new Iphone("4241241");
+fone.Ligar();
+fone.ReceberLigacao();
+fone.InstalarAplicativo("fire");
