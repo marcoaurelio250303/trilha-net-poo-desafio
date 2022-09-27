fe36d47b1adda32a84ed473e4e1c4f4a4c5a759f
marco aurélio <marcoaurelio250303@gmail.com>
2022-09-27 14:01:18 -0300

    implementado o desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public string Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            IMEI = imei;
+            Memoria = Memoria;
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
